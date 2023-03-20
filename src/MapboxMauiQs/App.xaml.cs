024870f1c6730e5a10fc2ab057985e535e9938f3
Tuyen Vu <hi@tuyen-vuduc.tech>
2023-03-21 04:30:34 +0700

    Initial

---
+++ src/MapboxMauiQs/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MapboxMauiQs;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ src/MapboxMauiQs/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MapboxMauiQs;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+

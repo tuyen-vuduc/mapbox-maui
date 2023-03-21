7a654d18ebf3e61cb614aa7fafa0a872969f8289
Tuyen Vu <hi@tuyen-vuduc.tech>
2023-03-21 07:29:32 +0700

    - [WIP] .net7 android

---
+++ src/Net7DroidQs/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Com.Mapbox.Maps;
+
+namespace Net7DroidQs;
+
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class MainActivity : Activity
+{
+    MapView mapView;
+
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        //// Set our view from the "main" layout resource
+        //SetContentView(Resource.Layout.activity_main);
+
+        mapView = new MapView(this);
+        SetContentView(mapView);
+        mapView.MapboxMap.LoadStyleUri(Style.MapboxStreets);
+    }
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+        mapView?.OnStart();
+    }
+
+    protected override void OnStop()
+    {
+        base.OnStop();
+        mapView?.OnStop();
+    }
+
+    public override void OnLowMemory()
+    {
+        base.OnLowMemory();
+        mapView?.OnLowMemory();
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        mapView?.OnDestroy();
+    }
+}

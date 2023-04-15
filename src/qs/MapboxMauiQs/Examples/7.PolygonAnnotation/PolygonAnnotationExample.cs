8931d2c9ec53f6cddf224c38ba8c620c04198870
Tuyen Vu <hi@tuyen-vuduc.tech>
2023-04-15 17:49:49 +0700

    - add PolygonAnnotationExample -- initial

---
+++ src/qs/MapboxMauiQs/Examples/7.PolygonAnnotation/PolygonAnnotationExample.cs
@@ -0,0 +1,38 @@
+using System;
+using Mapbox.Maui;
+using iOSPage = Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific.Page;
+namespace MapboxMauiQs;
+
+public class PolygonAnnotationExample : ContentPage, IExamplePage, IQueryAttributable
+{
+    MapboxView map;
+    IExampleInfo info;
+
+    public PolygonAnnotationExample()
+	{
+        iOSPage.SetUseSafeArea(this, false);
+		Content = map = new MapboxView();
+
+        map.MapReady += Map_MapReady;
+	}
+
+    private void Map_MapReady(object sender, EventArgs e)
+    {
+        var centerLocation = new Point(25.04579, -88.90136);
+        var cameraOptions = new CameraOptions
+        {
+            Center = centerLocation,
+            Zoom = 5,
+        };
+
+        map.CameraOptions = cameraOptions;
+
+    }
+
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        info = query["example"] as IExampleInfo;
+
+        Title = info?.Title;
+    }
+}
---
+++ src/qs/MapboxMauiQs/Examples/7.PolygonAnnotation/PolygonAnnotationExampleInfo.cs
@@ -0,0 +1,8 @@
+namespace MapboxMauiQs;
+class PolygonAnnotationExampleInfo : IExampleInfo
+{
+    public string Group => "Annotations";
+    public string Title => "Add a polygon annotation";
+    public string Subtitle => "Add a polygon annotation to the map";
+    public string PageRoute => typeof(PolygonAnnotationExample).FullName;
+}

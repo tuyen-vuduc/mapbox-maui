e6908c004a422aa9157bfa22ce2b5d64bb7a3030
Tuyen Vu <hi@tuyen-vuduc.tech>
2024-06-25 16:20:22 +0700

    - trigger fake build

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 dotnet nuget locals -c all
+
+# Build to trigger gradle process
+dotnet build -t:Clean,Rebuild src/qs/MapboxMauiQs/MapboxMauiQs.csproj
+
 dotnet pack -c Release -t:Clean,Rebuild mapbox-maui.sln --output $PWD/nugets

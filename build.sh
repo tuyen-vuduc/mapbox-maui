94c14cb702a982cba057d4ca40515fecf8d313aa
Tuyen Vu <hi@tuyen-vuduc.tech>
2024-06-25 16:47:50 +0700

    - try pass csproj property from cli

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 dotnet nuget locals -c all
 
 # Build to trigger gradle process
-dotnet build -t:Clean,Rebuild src/qs/MapboxMauiQs/MapboxMauiQs.csproj
+dotnet build -t:Clean,Rebuild src/qs/MapboxMauiQs/MapboxMauiQs.csproj \
+    -property:MAPBOX_DOWNLOADS_TOKEN=$MAPBOX_DOWNLOADS_TOKEN
 
 dotnet pack -c Release -t:Clean,Rebuild mapbox-maui.sln --output $PWD/nugets

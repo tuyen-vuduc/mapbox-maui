7cb02ea1b3c6dec18e869cb8ca15e9572ba63bb7
Tuyen Vu <tuyen.vuduc0902@gmail.com>
2024-06-28 06:41:30 +0700

    - update to use latest binding libs

--- src/libs/Mapbox.Maui/Platforms/iOS/MapboxViewHandler.Camera.cs
+++ src/libs/Mapbox.Maui/Platforms/iOS/MapboxViewHandler.Camera.cs
@@ -1,4 +1,5 @@
 ﻿using MapboxMapsObjC;
+using UIKit;
 
 namespace MapboxMaui;
 
@@ -11,9 +12,20 @@ partial class MapboxViewHandler : IMapCameraController
         if (mapView == null) return;
 
         var xcameraOptions = cameraOptions.ToNative();
+        var curve = animationOptions?.Curve is not null
+            ? animationOptions.Curve switch
+            {
+                TimingCurve.EaseIn => UIViewAnimationCurve.EaseIn,
+                TimingCurve.EaseInOut => UIViewAnimationCurve.EaseInOut,
+                TimingCurve.EaseOut => UIViewAnimationCurve.EaseOut,
+                TimingCurve.Linear => UIViewAnimationCurve.Linear,
+                _ => UIViewAnimationCurve.EaseOut,
+            }
+            : UIViewAnimationCurve.EaseOut;
         mapView.Camera().FlyTo(
             xcameraOptions,
             animationOptions?.Duration / 1000L ?? 0,
+            curve,
             (position) =>
             {
                 var xposition = position switch
@@ -34,9 +46,20 @@ partial class MapboxViewHandler : IMapCameraController
         if (mapView == null) return;
 
         var xcameraOptions = cameraOptions.ToNative();
+        var curve = animationOptions?.Curve is not null
+            ? animationOptions.Curve switch
+            {
+                TimingCurve.EaseIn => UIViewAnimationCurve.EaseIn,
+                TimingCurve.EaseInOut => UIViewAnimationCurve.EaseInOut,
+                TimingCurve.EaseOut => UIViewAnimationCurve.EaseOut,
+                TimingCurve.Linear => UIViewAnimationCurve.Linear,
+                _ => UIViewAnimationCurve.EaseOut,
+            }
+            : UIViewAnimationCurve.EaseOut;
         mapView.Camera().FlyTo(
             xcameraOptions,
             animationOptions?.Duration / 1000L ?? 0,
+            curve,
             (position) =>
             {
                 var xposition = position switch

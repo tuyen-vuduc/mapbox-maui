7c41f4ef2fb6a56b888ba77c0d35cb0526c40786
Tuyen Vu <tuyen.vuduc0902@gmail.com>
2024-07-04 07:51:52 +0700

    - empty example for gesture settings

---
+++ src/qs/MapboxMauiQs/Examples/Lab/66.GestureSettings/GestureSettingsExample.cs
@@ -0,0 +1,41 @@
+namespace MapboxMauiQs;
+
+public class GestureSettingsExample : ContentPage, IExamplePage, IQueryAttributable
+{
+    MapboxView map;
+    IExampleInfo info;
+
+    public GestureSettingsExample()
+	{
+        iOSPage.SetUseSafeArea(this, false);
+		Content = map = new MapboxView();
+
+        map.MapReady += Map_MapReady;
+        map.MapLoaded += Map_MapLoaded;
+	}
+
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        info = query["example"] as IExampleInfo;
+
+        Title = info?.Title;
+    }
+
+    private void Map_MapReady(object sender, EventArgs e)
+    {
+        var centerLocation = new MapPosition(21.0278, 105.8342);
+        var cameraOptions = new CameraOptions
+        {
+            Center = centerLocation,
+            Zoom = 14,
+        };
+
+        map.CameraOptions = cameraOptions;
+        map.MapboxStyle = MapboxStyle.MAPBOX_STREETS;
+    }
+
+    private void Map_MapLoaded(object sender, EventArgs e)
+    {
+        // Setup Styles, Annotations, etc here
+    }
+}

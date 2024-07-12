3dbb2acf549bf333b608ef92269d694d7f22c03b
Tuyen Vu <hi@tuyen-vuduc.tech>
2024-07-12 07:40:49 +0700

    - correct build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 dotnet nuget locals -c all
 
-dotnet pack -c Release -t:Clean,Rebuild src/libs/Mapbox.Maui/Mapbox.Maui.csproj \
+dotnet pack -c Release \
+    # -v d \
+    -t:Clean,Rebuild src/libs/Mapbox.Maui/Mapbox.Maui.csproj \
     --output $PWD/nugets

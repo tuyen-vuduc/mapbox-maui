d7ad662a04c6020a0939315bbda54b30262d3d7b
Tuyen Vu <tuyen.vuduc0902@gmail.com>
2024-08-27 05:24:46 +0700

    - align DslOperator

--- tools/generator/expression-factories.js
+++ tools/generator/expression-factories.js
@@ -22,7 +22,7 @@ function generateFactories() {
         .map(x => /^public static let (\w+)/.exec(x.trim())[1])
         .map(x => helper.pascalCase(x))
         .map(x => nameMapping[x] ?? x)
-        .map(x => `    public static DslExpression ${x}(params object[] arguments) => new(ExpressionOperator.${x}, arguments);`);
+        .map(x => `    public static DslExpression ${x}(params object[] arguments) => new(DslOperator.${x}, arguments);`);
 
     fs.writeFileSync(
             path.join(output, 'DslExpression.Factories.cs'), 
@@ -35,4 +35,36 @@ partial class DslExpression
             .concat(factories)
             .concat(['}'])
             .join('\n'));
+    
+        var operators = content.split('\n')
+            .filter(x => /^public static let (\w+)/.test(x.trim()))
+            .map(x => /^public static let (\w+).+("[^"]+")/.exec(x.trim()).slice(1, 3))
+            .map(x => [helper.pascalCase(x[0]), x[1]])
+            .map(x => [nameMapping[x[0]] ?? x[0], x[1]])
+            .map(x => `    public static readonly DslOperator ${x[0]} = new(${x[1]});`);
+    
+        fs.writeFileSync(
+                path.join(output, 'DslOperator.cs'), 
+                [
+                    `namespace MapboxMaui.Expressions;
+
+public struct DslOperator : INamedString
+{
+    public string Value { get; }
+
+    private DslOperator(string op)
+    {
+        Value = op;
+    }
+
+    public override string ToString() => Value;
+
+    public static implicit operator string(DslOperator value) => value.Value;
+    public static implicit operator DslOperator(string value) => new (value);
+`
+                ]
+                .concat(operators)
+                .concat(['}'])
+                .join('\n'));
+        
 }

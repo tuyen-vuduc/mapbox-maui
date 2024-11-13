962a7cc111ae34cc082a37fba21fa2ed0af99219
Tuyen Vu <hi@tuyen-vuduc.tech>
2024-11-14 06:04:20 +0700

    - move to new page from a map page

--- src/qs/MapboxMauiQs/Examples/09.BasicMap/BasicMapExample.xaml.cs
+++ src/qs/MapboxMauiQs/Examples/09.BasicMap/BasicMapExample.xaml.cs
@@ -5,12 +5,12 @@ public partial class BasicMapExample : ContentPage, IExamplePage, IQueryAttribut
     IExampleInfo info;
 
     public BasicMapExample()
-	{
-		InitializeComponent();
+    {
+        InitializeComponent();
         iOSPage.SetUseSafeArea(this, false);
 
         map.MapReady += Map_MapReady;
-	}
+    }
 
     private void Map_MapReady(object sender, EventArgs e)
     {
@@ -30,5 +30,31 @@ public partial class BasicMapExample : ContentPage, IExamplePage, IQueryAttribut
         info = query["example"] as IExampleInfo;
 
         Title = info?.Title;
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new NextTestPage());
+    }
+}
+
+class NextTestPage : ContentPage
+{
+    public NextTestPage()
+    {
+        Content = new StackLayout
+        {
+            Children = {
+                new Label {
+                    Text = "Demo moving to new page from a map page",
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    VerticalOptions = LayoutOptions.Center,
+                },
+                new Button {
+                    Text = "Back",
+                    HorizontalOptions = LayoutOptions.Center,
+                    Command = new Command(() => Navigation.PopAsync()), }
+            },
+        };
     }
 }
